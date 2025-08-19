ff632a28e19ca2735d7a747f8abd0ee2114d420c
LeeHeesang <gmltkd8012@naver.com>
2025-08-19 16:42:18 +0900

    [LeeCoder] Feat : Room Module Gradle 설정 및 기본 RoomDB 생성

--- core/room/build.gradle.kts
+++ core/room/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.library)
+    alias(libs.plugins.lib.room)
+    alias(libs.plugins.kotlin.serialization)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+
+
+android {
+    namespace = "com.rebuilding.muscleatlas.room"
 }
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+
+dependencies {
+
 }

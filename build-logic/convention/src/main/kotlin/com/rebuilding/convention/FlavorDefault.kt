7565cd36ff154b23d702ab1d94dcd70945416ac5
LeeHeesang <gmltkd8012@naver.com>
2025-08-19 09:44:21 +0900

    [LeeCoder] Feat : Build Logic Module 생성

---
+++ build-logic/convention/src/main/kotlin/com/rebuilding/convention/FlavorDefault.kt
@@ -0,0 +1,54 @@
+package com.rebuilding.convention
+
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.ProductFlavor
+import com.android.tools.r8.internal.St
+
+@Suppress("EnumEntryName")
+enum class ApiDimension {
+    server,
+    version,
+}
+
+enum class ApiHost {
+    BIZ,
+    DEV, // 없음
+    QA, // 없음
+}
+
+@Suppress("EnumEntryName")
+enum class ApiDimensionFlavor(
+    val dimension: ApiDimension,
+    val apiHost: String? = null,
+) {
+    ApiBiz(dimension = ApiDimension.server, apiHost = ApiHost.BIZ.name),
+    normal(dimension = ApiDimension.version),
+    servererror(dimension = ApiDimension.version),
+}
+
+val dimensionFlavorList = mutableListOf(ApiDimension.server.name, ApiDimension.version.name)
+
+fun configureFlavorDefault(
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
+    flavorConfigurationBlock: ProductFlavor.(flavor: ApiDimensionFlavor) -> Unit = {}
+) {
+    commonExtension.apply {
+        buildFeatures {
+            buildConfig = true
+        }
+
+        flavorDimensions.addAll(dimensionFlavorList)
+
+        productFlavors {
+            ApiDimensionFlavor.values().forEach {
+                create(it.name) {
+                    dimension = it.dimension.name
+                    flavorConfigurationBlock(this, it)
+                    if (!it.apiHost.isNullOrEmpty()) {
+                        buildConfigField("String", "API_HOST", "\"${it.apiHost}\"")
+                    }
+                }
+            }
+        }
+    }
+}

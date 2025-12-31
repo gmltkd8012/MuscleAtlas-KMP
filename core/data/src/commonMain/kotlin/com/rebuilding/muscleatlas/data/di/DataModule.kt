36dbf73778cdb23b299f3a169880f164e351b1e0
Leecoder <heesangdev@gmail.com>
2025-12-31 21:45:22 +0900

    [LeeCoder] Feat: Dispatchers 모듈 생성 MemberRepository IO 스레드에서 작업 하도록 수정

--- core/data/src/commonMain/kotlin/com/rebuilding/muscleatlas/data/di/DataModule.kt
+++ core/data/src/commonMain/kotlin/com/rebuilding/muscleatlas/data/di/DataModule.kt
@@ -5,5 +5,11 @@ import com.rebuilding.muscleatlas.data.repository.MemberRepositoryImpl
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<MemberRepository> { MemberRepositoryImpl(get()) }
+    includes(dispatcherModule)
+    single<MemberRepository> {
+        MemberRepositoryImpl(
+            supabaseClient = get(),
+            ioDispatcher = get(DispatcherQualifier.Io),
+        )
+    }
 }
